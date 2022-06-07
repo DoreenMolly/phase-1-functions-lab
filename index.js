053e274d83077229df46b93c0e41f227a956832c
Doreen <wanjirudoreen13@gmail.com>
2022-06-07 11:58:06 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution in this file!
+const hq = 42
+const distanceFromHqInBlocks = function (value){
+    
+    if (value <42){
+        return hq - value
+    }
+   return value - hq
+   }
+let distance = distanceFromHqInBlocks(1)
+
+function distanceFromHqInFeet (1){
+    console.log(distanceFromHqInBlocks(1)*264)
+}
