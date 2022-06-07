6f2fdb7a7ed13641684f5e2f8388f711e2e60c74
Doreen <wanjirudoreen13@gmail.com>
2022-06-07 23:33:22 +0300

    Done.

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 // Code your solution in this file!
-const hq = 42
-const distanceFromHqInBlocks = function (value){
-    
-    if (value <42){
-        return hq - value
-    }
-   return value - hq
-   }
-let distance = distanceFromHqInBlocks(1)
 
-function distanceFromHqInFeet (1){
-    console.log(distanceFromHqInBlocks(1)*264)
+const distanceFromHqInBlocks = (value) => Math.abs(42 - value);
+const distanceFromHqInFeet = (number) => 264 * distanceFromHqInBlocks(number);
+const distanceTravelledInFeet = (begin, end) => Math.abs(begin - end) * 264;
+const calculatesFarePrice = (begin, end) => {
+    let distance = distanceTravelledInFeet(begin, end);
+    if(distance <= 400){
+        return 0;
+    }else if(distance > 400 && distance <= 2000){
+        return (distance - 400) * 0.02;
+    }else if(distance > 2000 && distance < 2500){
+        return 25;
+    }else if(distance >= 2500){
+        return "cannot travel that far";
+    }
 }
+
+console.log(calculatesFarePrice(20, 54));
+
